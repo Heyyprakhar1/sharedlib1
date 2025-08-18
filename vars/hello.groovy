fb061c3912274b65b01a76becdebaf4c75eb7524
Heyyprakhar1 <Srivprak0106@gmail.com>
2025-08-18 10:11:58 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -7,3 +7,11 @@ def product(a,b)
 {
   echo "The product of ${a} and ${b} is ${a*b}"
 }
+def subtraction(a,b)
+{
+  echo "The difference of ${a} and ${b} is ${a-b}"
+} 
+def division(a,b)
+{
+  echo "The divide of ${a} and ${b} is ${a/b}"
+}
